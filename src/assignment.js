65d5a6169d6da166c71b7ced363c8f17439a92c9
ngipe jim <>
2022-07-10 19:27:19 +0000

    assignment submit

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    arrayOfNumbers.forEach((number) =>{
+      sum = sum + number;
+    })
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let totalEvenNumbers = 0;
+    arrayOfNumbers.forEach((number) =>{
+      
+      if (number%2 === 0){
+        totalEvenNumbers = totalEvenNumbers + 1;
+      }
+    })
+    return totalEvenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
